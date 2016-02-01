bd0a0d7761e4c80224a4f50db86e8b3d37a6aa23
Stefan Aichholzer <theaichholzer@gmail.com>
2016-02-01 12:41:04 +0100

    Missing the Gruntfile..?

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+'use strict';
+
+
+require('dotenv').load();
+module.exports = function (grunt) {
+
+    process.env.NODE_ENV = grunt.option('env') || 'local';
+    if (grunt.option('with') === 'app') {
+        require('./app')
+    }
+
+    grunt.initConfig({
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            cometa: [
+                '.'
+            ]
+        },
+        mocha: {
+            test: {
+                options: {
+                    reporter: grunt.option('reporter') || process.env.reporter || 'nyan',
+                    slow: grunt.option('slow') || 75,
+                    timeout: grunt.option('timeout') || 25000,
+                    quiet: false,
+                    bail: true,
+                    'check-leaks': true
+                },
+                src: ['test/index.js']
+            }
+        },
+        nodemon: {
+            script: 'app/index.js',
+            options: {
+                callback: function (nodemon) {
+                    nodemon.on('log', function (event) {
+                        console.log(event.colour);
+                    });
+                },
+                env: {
+                    NODE_ENV: process.env.NODE_ENV || 'local',
+                    PORT: process.env.PORT
+                },
+                cwd: __dirname,
+                ignore: ['node_modules/**'],
+                ext: 'js',
+                delay: 1000
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-nodemon');
+
+    grunt.registerTask('default', 'All default tasks', ['jshint:cometa', 'mocha']);
+    grunt.registerTask('hint', 'Hinting codebase', 'jshint:cometa');
+    grunt.registerTask('test', 'Run tests', 'mocha');
+
+};

772d1173b7b2c75528b390cd60a22ef7ef08915d
Stefan Aichholzer <theaichholzer@gmail.com>
2016-02-28 10:24:02 +0100

    Baby, let cluster this thing!

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
-    'use strict';
+'use strict';
 
 
-let express = require('express'),
+let cluster = require('cluster'),
+    cpus = require('os').cpus(),
+    express = require('express'),
     router = require('./library/router'),
     favicon = require('serve-favicon'),
     app = express();
@@ -19,6 +21,18 @@ app.use(
     }
 );
 
-app.listen(process.env.PORT, () => {
-    console.log('Up on port:', process.env.PORT);
-});
+if (cluster.isMaster) {
+    for (let cpu = 0; cpu < cpus.length; cpu += 1) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+        cluster.fork();
+    });
+
+} else {
+    app.listen(process.env.PORT, () => {
+        console.log('Up on port:', process.env.PORT);
+    });
+}

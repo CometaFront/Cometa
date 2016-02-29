d85b665489665b2c8e8370296b60f4e82ade8361
Stefan Aichholzer <theaichholzer@gmail.com>
2016-02-29 17:12:36 +0100

    Simplified parse.

--- app/library/parse.js
+++ app/library/parse.js
@@ -5,7 +5,9 @@ module.exports = req => {
 
     let supportedOutput = ['webp', 'png', 'jpeg'],
         path = '',
-        provider = null;
+        provider = null,
+        outputFormat = 'jpeg',
+        outputQuality = parseInt(req.query.q || req.query.quality);
 
     if (req.params[0]) {
         path = req.params[0];
@@ -20,20 +22,16 @@ module.exports = req => {
     // TODO: Error on invalid input - imageParts[1]
     // let supportedInput = ['webp', 'png', 'jpeg', 'jpg']
 
-    let outputFormat = 'jpeg';
     if (imageParts[2] && supportedOutput.indexOf(imageParts[2]) >= 0) {
         outputFormat = imageParts[2];
         path = path.replace('.' + outputFormat, '');
     }
 
-    let outputQuality = parseInt(req.query.q || req.query.quality);
-    outputQuality = outputQuality > 0 && outputQuality <= 100 ? outputQuality : 80;
-
     return {
         output: {
             width: parseInt(req.query.w || req.query.width) || 0,
             height: parseInt(req.query.h || req.query.height) || 0,
-            quality: outputQuality,
+            quality: outputQuality > 0 && outputQuality <= 100 ? outputQuality : 80,
             format: outputFormat
         },
         imagePath: path || null,

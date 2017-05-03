522bbac1cd92e3e420fda8c29248f286f1f23b4e
Stefan Aichholzer <theaichholzer@gmail.com>
2017-05-03 17:59:36 +1000

    Not sure where these ones went...

---
+++ app/core/streams/response.js
@@ -0,0 +1,44 @@
+'use strict';
+
+
+const crypto = require('crypto');
+const sharp = require('sharp');
+const Writable = require('stream').Writable;
+
+module.exports = (res, next) => new Writable({
+    objectMode: true,
+    write: async (image, encoding, callback) => {
+
+        try {
+            let options = { quality: image.output.quality };
+            if (image.output.extension === 'png') {
+                options = { compressionLevel: 5 };
+            }
+
+            setImmediate(() => {
+                sharp(image.body)[image.output.extension](options).once('info', info => {
+
+                    const expirationDate = new Date();
+                    expirationDate.setDate(expirationDate.getDate() + 30);
+
+                    const headers = {
+                        'Content-Type': `image/${image.output.extension}`,
+                        'Cache-Control': 'max-age=2592000,public',
+                        'Expires': expirationDate.toGMTString(),
+                        'Content-Length': info.size,
+                        'Last-Modified': (new Date()).toGMTString(),
+                        'Etag': crypto.createHash('sha1').update(image.body, 'utf8').digest('hex'),
+                        'Vary': 'Accept-Encoding'
+                    };
+
+                    res.set(headers);
+                    image = null;
+
+                }).pipe(res);
+                callback();
+            });
+        } catch (error) {
+            callback(error);
+        }
+    }
+}).once('error', next);
